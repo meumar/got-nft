1c59caee5f98f69e518af17b5cda3ed1e5d5c78d
Umar sheik <meumar@Umars-MacBook-Air.local>
2024-08-29 10:23:51 +0530

    second

--- src/app/components/Layout/Topbar.tsx
+++ src/app/components/Layout/Topbar.tsx
@@ -5,7 +5,7 @@ export default function Topbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 h-100%">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             GOT-NFT
           </span>
